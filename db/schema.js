8446f8f7d299845b3324f39543b9f24a38a7df25
dp_chua <dp_chua@hotmail.com>
2017-03-06 18:48:43 -0500

    schema sorta type

---
+++ db/schema.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+
+var UserSchema = new Schema({
+  name: String,
+  email: String,
+  pasword_digest: String,
+  books: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Book'
+  }]
+};
+
+var BookSchema = new Schema({
+  name: String,
+  author: String,
+  publisher: String,
+  book_url: String,
+  date_published: String,
+  onwer: {
+    type: String,
+    ref: 'User'
+  }
+});
+
+UserSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+
+  if (!this.created_at){
+    this.created_at = now
+  }
+  next()
+});
+
+BookSchema.pre('save', function(next){
+  now = new Date();
+  this.updated_at = now;
+
+  if (!this.created_at) {
+    this.created_at = now
+  }
+  next()
+});
+
+var UserModel = mongoose.model('User', UserSchema);
+var BookModel = mongoose.model('Book', BookSchema);
+
+module.exports = {
+  Book: BookModel,
+  User: UserModel
+}
+
+
+})
