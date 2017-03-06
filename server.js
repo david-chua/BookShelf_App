90389755d5a816fbed28360b1b0b18d8f6ca9fce
dp_chua <dp_chua@hotmail.com>
2017-03-06 17:27:58 -0500

    server setup

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 
+var app = epress();
+
+app.use(express.static('public'));
+
+var mongoURI = process.env.MONGODB_URI || "mongodb://localhost/bookshelf_app"
+mongoose.connect(mongoURI);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.use(logger('dev'));
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log("My Book Collection Works!!");
+});
