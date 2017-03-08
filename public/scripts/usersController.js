7e7b13573f865515bb016118f064e05039088059
dp_chua <dp_chua@hotmail.com>
2017-03-08 14:22:04 -0500

    trying to get users controller to work

--- public/scripts/usersController.js
+++ public/scripts/usersController.js
@@ -2,5 +2,30 @@ angular.module('bookApp')
   .controller('usersController', usersController)
 
 function usersController($http, $state, $scope){
+  var self = this;
+
   console.log('usersController is working')
+
+  function signup(user){
+    console.log("sign me up")
+    console.log(user)
+    $http.post('/users', user)
+      .then(function(response){
+        console.log(response)
+        $state.go('login');
+      })
+  }
+  function login(user){
+    console.log(user);
+    $http.post('/sessions/login', user)
+    .then(function(response){
+      $scope.currentUser = response.data;
+      console.log(response.data)
+
+    })
+  }
+
+  self.singup = signup;
+
+
 }
