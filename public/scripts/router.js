07cf8a0cb1b7c93d3dae1c03bd34dfed97a56a2b
dp_chua <dp_chua@hotmail.com>
2017-03-08 02:54:58 -0500

    partial pages created

--- public/scripts/router.js
+++ public/scripts/router.js
@@ -5,4 +5,29 @@ BookAppRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
 
 function BookAppRouter($stateProvider, $urlRouterProvider){
   console.log("app router is working");
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: '/partials/home.html'
+    })
+    .state('signup',{
+      url:'/signup',
+      templateUrl: '/partials/signup.html'
+    })
+    .state('login',{
+      url: '/login',
+      templateUrl: '/partials/login.html'
+    })
+    .state('usershow',{
+      url: '/users',
+      templateUrl: '/partials/usershow.html'
+    })
+    .state('bookshow',{
+      url:'/users/:userId/books'
+      templateUrl: '/partials/bookshow.html'
+    })
+    .state('contact',{
+      url:'/contact',
+      templateUrl: '/partials/contact.html'
+    })
 };
